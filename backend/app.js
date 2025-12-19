71af685c14d2ce65f9ee536df42d25903923b8a8
Raja-r-art <raja.r2024aiml@sece.ac.in>
2025-12-19 09:31:21 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,12 @@ const authRoutes = require("./routes/authRoutes");
 const app = express();
 
 // middleware
-app.use(cors());
+app.use(cors(
+    {
+    origin:"http://13.60.208.47:5000",
+    credentials:true
+ }
+));
 app.use(express.json());
 app.use("/api/products", productRoutes);
 app.use("/api/cart", cartRoutes);
